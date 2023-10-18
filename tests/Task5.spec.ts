d79e60a0f18c3aa25fcfc7236fc76ea33f639cc9
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-10-18 07:49:19 +0200

    updating

--- tests/Task5.spec.ts
+++ tests/Task5.spec.ts
@@ -1,6 +1,6 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox';
 import { toNano } from 'ton-core';
-import { Task5 } from '../wrappers/Task5';
+import { Task5, AdminWithdrawalProfit } from '../wrappers/Task5';
 
 describe('Task5', () => {
     let blockchain: Blockchain;
@@ -8,29 +8,34 @@ describe('Task5', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-        task5 = blockchain.openContract(await Task5.fromInit());
+        const wallets: SandboxContract<TreasuryContract>[] = await blockchain.createWallets(1);
         const deployer = await blockchain.treasury('deployer');
+        task5 = blockchain.openContract(await Task5.fromInit(BigInt(432), deployer.address));
         const deployResult = await task5.send(
             deployer.getSender(),
             {
-                value: toNano('0.05'),
+                value: toNano('100'),
             },
             {
                 $$type: 'Deploy',
                 queryId: 0n,
             }
         );
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: task5.address,
-            deploy: true,
-            success: true,
-        });
+
+        console.log(
+            await task5.send(wallets[0].getSender(), { value: BigInt(1000000) }, {
+                $$type: 'AdminWithdrawalProfit',
+                queryId: BigInt(0),
+            } as AdminWithdrawalProfit)
+        );
+        console.log(await task5.getProfit());
+        // expect(deployResult.transactions).toHaveTransaction({
+        //     from: deployer.address,
+        //     to: task5.address,
+        //     deploy: true,
+        //     success: true,
+        // });
     });
 
-    it('test', async () => {
-    });
+    it('test', async () => {});
 });
-
-
-
